12129d39a866fad8bd81823d9ed2aa96a0f52917
Adam Smith <zero1three@gmail.com>
2016-12-04 23:33:59 -0800

    add HTTP Server + redirection

--- redirector.go
+++ redirector.go
@@ -1,40 +1,49 @@
 package main
 
 import (
-    "fmt"
-    "gopkg.in/yaml.v2"
-    "io/ioutil"
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
 )
 
 type Config struct {
-    Server    Server
-    Redirects []Redirect
+	Server    Server
+	Redirects map[string]map[string]string
 }
 
 type Server struct {
-    Address string `yaml:"address"`
-	  Port    int    `yaml:"port"`
-}
-
-type Redirect struct {
-    From string `yaml:"from"`
-    To   string `yaml:"to"`
-    With int    `yaml:"with"`
+	Address string `yaml:"address"`
+	Port    int    `yaml:"port"`
 }
 
 func main() {
-    c, err := ioutil.ReadFile("config.yaml")
-    if err != nil {
-        panic(err)
-    }
+	c, err := ioutil.ReadFile("config.yaml")
+	if err != nil {
+		log.Fatal("Error reading config: ", err)
+	}
 
-    config := Config{}
-    err = yaml.Unmarshal(c, &config)
-    if err != nil {
-        panic(err)
-    }
+	config := Config{}
+	err = yaml.Unmarshal(c, &config)
+	if err != nil {
+		log.Fatal("Error marshalling YAML: ", err)
+	}
 
-    for _, redirect := range config.Redirects {
-        fmt.Printf("From: %v, To: %v, With: %v\n", redirect.From, redirect.To, redirect.With)
-    }
+	// http.Request.Host
+	// RedirectHandler
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		to := config.Redirects[r.Host]["to"]
+		with, _ := strconv.Atoi(config.Redirects[r.Host]["with"])
+
+		http.Redirect(w, r, to, with)
+	})
+
+	fmt.Printf("Listening on %v:%v\n", config.Server.Address, config.Server.Port)
+	err = http.ListenAndServe(fmt.Sprintf("%v:%v", config.Server.Address, config.Server.Port), nil)
+	if err != nil {
+		log.Fatal("Error starting server: ", err)
+	}
+
 }
