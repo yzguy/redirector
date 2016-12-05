e8249e88733b8dcbacdb2f74d42e7aa99929a457
Adam Smith <zero1three@gmail.com>
2016-12-04 23:36:17 -0800

    comments

--- redirector.go
+++ redirector.go
@@ -20,19 +20,20 @@ type Server struct {
 }
 
 func main() {
+	// Read in config file
 	c, err := ioutil.ReadFile("config.yaml")
 	if err != nil {
 		log.Fatal("Error reading config: ", err)
 	}
 
+	// Unmarshal YAML
 	config := Config{}
 	err = yaml.Unmarshal(c, &config)
 	if err != nil {
 		log.Fatal("Error marshalling YAML: ", err)
 	}
 
-	// http.Request.Host
-	// RedirectHandler
+	// Setup handler for redirects
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		to := config.Redirects[r.Host]["to"]
 		with, _ := strconv.Atoi(config.Redirects[r.Host]["with"])
@@ -40,10 +41,10 @@ func main() {
 		http.Redirect(w, r, to, with)
 	})
 
+	// Start HTTP Server
 	fmt.Printf("Listening on %v:%v\n", config.Server.Address, config.Server.Port)
 	err = http.ListenAndServe(fmt.Sprintf("%v:%v", config.Server.Address, config.Server.Port), nil)
 	if err != nil {
 		log.Fatal("Error starting server: ", err)
 	}
-
 }
