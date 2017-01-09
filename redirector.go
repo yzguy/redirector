e0dd10335b013263992667ea5907d49dee94b24a
Adam Smith <zero1three@gmail.com>
2017-01-08 21:33:25 -0800

    handle not found redirect

--- redirector.go
+++ redirector.go
@@ -39,16 +39,28 @@ func main() {
 
 	// Setup handler for redirects
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    _, found := config.Redirects[r.Host]
+
+    if !found {
+      log.Printf("Client %s %s -> Not Found (404)", r.RemoteAddr, r.Host)
+      http.NotFound(w, r)
+      return
+    }
+
 		To := config.Redirects[r.Host].To
 		With := config.Redirects[r.Host].With
 
 		log.Printf("Client: %s %s -> %s (%d)\n", r.RemoteAddr, r.Host, To, With)
 		http.Redirect(w, r, To, With)
+    return
 	})
 
 	// Start HTTP Server
-	fmt.Printf("Listening on %v:%v\n", config.Server.Address, config.Server.Port)
-	err = http.ListenAndServe(fmt.Sprintf("%v:%v", config.Server.Address, config.Server.Port), nil)
+	ServerAddress := config.Server.Address
+	ServerPort := config.Server.Port
+
+	log.Printf("Listening on %v:%v\n", ServerAddress, ServerPort)
+	err = http.ListenAndServe(fmt.Sprintf("%v:%v", ServerAddress, ServerPort), nil)
 	if err != nil {
 		log.Fatal("Error starting server: ", err)
 	}
